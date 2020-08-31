5034c622cdd026a4e904f0e2cafaf853ff545c61
Christopher Perez <christopherperez@Christophers-MacBook-Air.local>
2020-08-30 23:37:16 -0500

    toggle state projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'react-mdl'
+import {Tabs, Tab, Grid, Cell} from 'react-mdl'
 
 
 class Projects extends Component {
@@ -7,6 +7,23 @@ class Projects extends Component {
         super(props);
         this.state = { activeTab: 0 }
     }
+    toggleCatergories() {
+         if(this.state.activeTab === 0) {
+        return(
+            <div><h1> this is react</h1></div>
+        )
+    } else if(this.state.activeTab === 1) {
+        return(
+            <div><h1>this is python</h1></div>
+        )
+    } else if(this.state.activeTab === 2) {
+        return (
+            <div><h1>this is mongodb</h1></div>
+        )
+    }
+    }
+  
+
     render() {
         return(
             <div className="category-tabs">
@@ -15,6 +32,14 @@ class Projects extends Component {
                     <Tab>Python</Tab>
                     <Tab>MongoDB</Tab>
                 </Tabs>
+                <section className="projects-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+        <div className="content">{ this.toggleCatergories() }</div>
+                        </Cell>
+                    </Grid>
+                {this.toggleCatergories()}
+                </section>
             </div>
         )
     }
